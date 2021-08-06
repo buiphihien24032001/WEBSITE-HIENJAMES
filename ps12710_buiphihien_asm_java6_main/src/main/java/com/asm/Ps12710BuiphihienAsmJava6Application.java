2c58b143cc07a14a4ac495b1d3748d01aa1b972a
Hiền Bùi <buiphihien24032001@gmail.com>
2021-08-06 09:02:26 +0700

    upload project

---
+++ ps12710_buiphihien_asm_java6_main/src/main/java/com/asm/Ps12710BuiphihienAsmJava6Application.java
@@ -0,0 +1,13 @@
+package com.asm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps12710BuiphihienAsmJava6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps12710BuiphihienAsmJava6Application.class, args);
+	}
+
+}
